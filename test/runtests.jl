580da2a4b0f66f79c1eb631bc06c2808ec70ef9d
bgrovep <bgrovepalt@gmail.com>
2026-03-03 13:06:29 +0100

    Altered docstring for localancestry and removed tests for time and RAM

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,16 +18,3 @@ packageroot = dirname(dirname(pathof(LocalAncestry)))
     @test sum(a.pcorrect[a.ncorrect .> 0]/sum(a.ncorrect .> 0)) > 0.88
 end
 
-
-@testset "Time" begin
-    dataset = "hapmap3"
-    _, time, _, _, _ = @timed localancestry("data/current_$(dataset)_2000_1.vcf.gz", "data/target_$(dataset)_2000_1.vcf.gz", "data/ga_own_1.csv")
-    @test time < 30.0
-end
-
-
-@testset "RAM" begin
-    dataset = "hapmap3"
-    _, _, bytes, _, _ = @timed localancestry("data/current_$(dataset)_2000_1.vcf.gz", "data/target_$(dataset)_2000_1.vcf.gz", "data/ga_own_1.csv")
-    @test bytes < 3*10^9
-end
